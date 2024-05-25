9d9abd3a1f215252fcfce0b1f7e08573bf53a052
rifahad <rifahadmt202@gmail.com>
2024-05-25 18:51:34 +0530

    Components creating

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Navigation from './Components/Footer'
+
+
+
+function App() {
+
+  return (
+    <>
+  <Navigation/>
+  {/* <NavbarSimple/> */}
+    </>
+  )
+}
+
+export default App
