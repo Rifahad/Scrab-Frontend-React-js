f40f30a029fc138b4c47baa01d73a86f27f16567
rifahad <rifahadmt202@gmail.com>
2024-08-30 01:47:53 +0530

    config changed

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // This makes the server accessible externally
-    port: 5173,      // You can also specify the port if needed
-  },});
+})
